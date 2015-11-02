bca1837ca358c78bf2745e61a24e6feef8257d72
Phil Chrapka <pchrapka@gmail.com>
2015-11-01 19:20:21 -0500

    update choco script

--- windows/choco-setup.bat
+++ windows/choco-setup.bat
@@ -3,27 +3,28 @@ rem Must be run as Admin
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
 rem Dev
-choco install wget
-choco install curl
+choco install -y wget
+choco install -y curl
 rem choco install git
-choco install expresso
-choco install imagemagick
+choco install -y expresso
+choco install -y imagemagick
 
 rem Apps
-choco install google-chrome-x64
+choco install -y google-chrome-x64
 rem issue with chrome double check
-choco install lastpass
-choco install javaruntime
+choco install -y lastpass
+choco install -y javaruntime
 rem choco install calibre
-choco install notepadplusplus.install
-choco install vlc
-choco install skype
+choco install -y notepadplusplus.install
+choco install -y vlc
+choco install -y skype
 rem choco install dropbox
-choco install teamviewer
-choco install winrar
-choco install FoxitReader
-choco install f.lux
+choco install -y teamviewer
+choco install -y winrar
+choco install -y FoxitReader
+choco install -y f.lux
 rem choco install avastfreeantivirus
+choco install -y steam
 
 rem Network
 choco install winscp
